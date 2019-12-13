2925a9ad84288f0ca798c6b486395ab8d3989a61
Jascha Riedel <jaschelite@googlemail.com>
2019-12-13 10:31:49 +0100

    Added simple documenation

--- rximp/api.py
+++ rximp/api.py
@@ -12,6 +12,14 @@ from .message import RxImpMessage
 class RxImp(object):
 
     def __init__(self, inObs: Observable, outSubject: Subject):
+        """
+        Parameters
+        ---------
+        inObs : Observable<bytes>
+            Observable<bytes> that the instance subscribes to in order to receive data packets. The Observable should emit objects of type bytes
+        outSubject : Subject<bytes>
+            Subscribe to the outSubject to publish messages (i. e. send the to the receiver)
+        """
         super().__init__()
         self._in: Observable = inObs.pipe(
             map(self._mapIncoming),
@@ -21,6 +29,20 @@ class RxImp(object):
         self._out.pipe(map(self._mapOutgoing)).subscribe(outSubject)
 
     def observableCall(self, topic: str, payload) -> Observable:
+        """
+        Parameters
+        ---------
+        topic : str
+            Topic mapped to this call. Other side must register a handler for this topic first using 'registerCall'
+        payload : any
+            Payload will be send to the other side in JSON format. May be None!
+        
+        Returns
+        ---------
+        Observable : dict
+            Observable that will emit items received from the other side. Will also emit termination events
+
+        """
         message = RxImpMessage(
             topic, 0, RxImpMessage.STATE_SUBSCRIBE, json.dumps(payload))
 
@@ -47,7 +69,19 @@ class RxImp(object):
         return create(subscriptionFunction)
 
     def registerCall(self, topic: str, handler: Callable[[Dict], Observable]) -> Disposable:
-
+        """
+        Parameters
+        ---------
+        topic : str
+            Topic this call will be registered on
+        handler : Callable[[Dict], Observable]
+            Handler for this topic. Is called with payload provided by caller (maybe empty). Return an Observable handling this call
+        
+        Returns
+        ---------
+        Disposable
+            To remove registration
+        """
         def handleSubscription(msg: RxImpMessage):
 
             def on_next(next):
